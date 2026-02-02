d8482bbc876ed98c142c7d22a2ae49001fa2c807
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-02 18:19:07 +0545

    register navbar in app.jsx

--- job-portal-frontend/src/App.jsx
+++ job-portal-frontend/src/App.jsx
@@ -3,15 +3,20 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import JobList from "./pages/jobs/JobList";
 import PrivateRoute from "./routes/PrivateRoute";
+import Navbar from "./components/Navbar";
+
 
 
 export default function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/list" element={<JobList />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       {/* <Route path="/my-jobs" element={<PrivateRoute> <MyJobs /> </PrivateRoute>} /> */}
     </Routes>
+    </>
   );
 }
