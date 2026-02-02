55bd3bc7f3c3a1c1a2decbe58aa36d64423ddb52
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-02 17:50:03 +0545

    fix dom error

--- job-portal-frontend/src/main.jsx
+++ job-portal-frontend/src/main.jsx
@@ -1,12 +1,12 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./auth/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-// createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
