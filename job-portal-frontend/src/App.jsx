365bf273abcc1c064f33a947f721f50892ee2b1c
AshrayaBashyal <ashrayabashyal1@gmail.com>
2026-02-02 17:41:12 +0545

    feat: add PrivateRoute guard for protected frontend routes

--- job-portal-frontend/src/App.jsx
+++ job-portal-frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import JobList from "./pages/jobs/JobList";
+import PrivateRoute from "./routes/PrivateRoute";
+
 
 export default function App() {
   return (
@@ -9,6 +11,7 @@ export default function App() {
       <Route path="/list" element={<JobList />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/my-jobs" element={<PrivateRoute> <MyJobs /> </PrivateRoute>} />
     </Routes>
   );
 }
